3d52ce275b5e9367c426dacd3b8d28dedc68cd16
houris <belg.faycel@gmail.com>
2017-09-24 21:43:45 +0200

    first commit

---
+++ Models/UserDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatApplication.Models
+{
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+        public int UserType { get; set; }
+    }
+}
